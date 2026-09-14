2ccdbf13116eec3b0a6c45f378a41ae67695b072
Mikesweeney55 <Mikesweeney55@yahoo.com>
2026-09-13 21:46:17 -0400

    Load consolidated Platoon display fixes

--- dev-mobile-pregame-fix.js
+++ dev-mobile-pregame-fix.js
@@ -1,4 +1,6 @@
 (()=>{
 'use strict';
-const s=document.createElement('script');s.src='dev-mobile-pregame-fix-core.js?v=3';document.head.appendChild(s);
+for(const src of ['dev-mobile-pregame-fix-core.js?v=3','platoon-display-fixes.js?v=1']){
+  const s=document.createElement('script');s.src=src;document.head.appendChild(s);
+}
 })();
